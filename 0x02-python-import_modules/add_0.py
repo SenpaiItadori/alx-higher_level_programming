66767689e17cdbb7bcd852fff81c850e2c5623b6
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-03-09 01:35:09 -0800

    add callomg

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        A + b
+    """
+    return (a + b)
