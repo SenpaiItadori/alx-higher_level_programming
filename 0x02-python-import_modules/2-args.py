afb37598f7085815358ea0b12c1a663b990db86e
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2024-01-18 01:45:22 -0800

    calc

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-argv = sys.argv
-argc = len(argv)
+    argv = sys.argv
+    argc = len(argv)
 
-if argc <= 1:
-    print("0 arguments.")
-elif argc == 2:
-    print("{:d} argument:".format(argc - 1))
-else:
-    print("{:d} arguments:".format(argc - 1))
-for i in range(1, len(argv)):
-    print("{:d}: {:s}".format(i, argv[i]))
+    if argc <= 1:
+        print("0 arguments.")
+    elif argc == 2:
+        print("{:d} argument:".format(argc - 1))
+    else:
+        print("{:d} arguments:".format(argc - 1))
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
