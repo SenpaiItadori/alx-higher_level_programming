a1715c15cf4dd62d117acb8327bb1c3b9632eeba
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2024-01-18 00:49:12 -0800

    infinte add in py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,8 @@ argc = len(argv)
 if argc <= 1:
     print("0 arguments.")
 elif argc == 2:
-    print("{} argument:".format(argc - 1))
+    print("{:d} argument:".format(argc - 1))
 else:
-    print("{} arguments:".format(argc - 1))
+    print("{:d} arguments:".format(argc - 1))
 for i in range(1, len(argv)):
-    print("{}: {}".format(i, argv[i]))
+    print("{:d}: {:s}".format(i, argv[i]))
