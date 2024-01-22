bb05f97457c4fe1410b156369200f8b20e867dd0
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2024-01-22 02:08:45 -0800

    Tuple addition

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in range(0, len(i)):
-            if j != len(i) - 1:
-                print("{:d}".format(i[j]), end=", ")
+        for j in i:
+            if j != i[-1]:
+                print("{:d}".format(j), end=", ")
             else:
-                print("{:d}".format(i[j]), end="")
-        print("")
+                print("{:d}".format(j))
