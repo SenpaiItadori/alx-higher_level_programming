dda0d54941a0c62c84538b1ac0c3cc80e4ccc7ca
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2024-01-23 00:41:36 -0800

    More returns

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if j != i[-1]:
-                print("{:d}".format(j), end=", ")
+                print("{:d}".format(j), end=" ")
             else:
                 print("{:d}".format(j))
