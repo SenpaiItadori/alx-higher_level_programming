53d91ebe1d049cf631e677ad40da9aec91648e1f
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2024-01-24 11:53:19 -0800

    debug processes

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    new = my_list.copy()
-
-    for i in range(len(my_list)):
-        new = list(map(lambda x: x * number, my_list))
-    return (new)
+    return (list(map(lambda x: x * number, my_list)))
