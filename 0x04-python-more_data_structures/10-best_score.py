24a48418aa3e79e6e4dab5aab9297d2e74693068
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2024-01-24 11:49:23 -0800

    debug processes

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    val = 0
-
-    if a_dictionary == None:
-        return (None)
-
-    for key in a_dictionary:
-        if val < a_dictionary[key]:
-            val = a_dictionary[key]
-    for key in a_dictionary:
-        if val == a_dictionary[key]:
-            return (key)
+    return (max(a_dictionary, key=a_dictionary.get) if a_dictionary else None)
