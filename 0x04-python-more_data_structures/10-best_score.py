12500bdb74901688a7de0965480ff32308c47b2f
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2024-01-24 11:46:21 -0800

    debug processes

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
+    val = 0
+
     if a_dictionary == None:
         return (None)
-    val = 0
+
     for key in a_dictionary:
         if val < a_dictionary[key]:
             val = a_dictionary[key]
