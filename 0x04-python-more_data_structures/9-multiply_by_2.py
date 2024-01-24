ee4ad20e813b94b1ef1189016d4b6afe78b138bd
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2024-01-24 11:43:19 -0800

    debug processes

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    A = a_dictionary.copy()
-    for key in A:
-        A[key] = A[key] * 2
-    return A
+    return ({key: val*2 for key, val in a_dictionary.items()})
